a6804b7e06d01ae7fd11693962289cc33927d1d8
Martijn Weverling <mweverli@student.codam.nl>
2023-06-05 19:54:43 +0200

    made philo_printer in separate thread and

--- src/philo/philo_message.c
+++ src/philo/philo_message.c
@@ -6,7 +6,7 @@
 /*   By: mweverli <mweverli@student.codam.n>          +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/05/26 21:15:26 by mweverli      #+#    #+#                 */
-/*   Updated: 2023/06/03 18:56:34 by mweverli      ########   odam.nl         */
+/*   Updated: 2023/06/05 19:50:08 by mweverli      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,47 +42,41 @@ static int32_t	printer_switch_queue(t_msg_queue *queue)
 	return (msg_count);
 }
 
-void	philo_printer(t_msg_queue *queue)
+void	*philo_printer(void *ptr)
 {
+	t_msg_queue	*queue;
 	int32_t		max_count;
 	int32_t		i;
 
-	return ;
+	queue = ptr;
 	while (1)
 	{
 		i = 0;
 		pthread_mutex_lock(&queue->msg_mutex);
 		max_count = printer_switch_queue(queue);
 		pthread_mutex_unlock(&queue->msg_mutex);
-		printf("\t-=- SWAPPED -=-\n");
 		while (i < max_count)
 		{
 			printf(FORMAT_MSG, queue->time[0][i], queue->philo[0][i], \
 				g_msg[queue->action[0][i]]);
 			if (queue->action[0][i] == DIE || queue->action[0][i] == END)
-				return ;
+				return (NULL);
 			i++;
 		}
-		time_sleep_ms(400);
-		ph_memset(queue->time[0], 0, sizeof(int32_t) * max_count);
-		ph_memset(queue->philo[0], 0, sizeof(int32_t) * max_count);
-		ph_memset(queue->action[0], 0, sizeof(int32_t) * max_count);
+		time_sleep_ms(1);
 	}
+	return (NULL);
 }
-// maybe add following line after the while loop containing the printf:
-//		time_sleep_ms(250);// this sleep may need some adjusting/fidling might even be unnecesary
 
 void	philo_queue_message(t_philo *philo, int32_t time, t_msg msg)
 {
 	int32_t		count;
-	t_msg_queue	*queue;
 
-	queue = philo->queue;
-	pthread_mutex_lock(&queue->msg_mutex);
-	count = queue->count;
-	queue->time[1][count] = time;
-	queue->philo[1][count] = philo->philo_id;
-	queue->action[1][count] = msg;
-	(queue->count)++;
-	pthread_mutex_unlock(&queue->msg_mutex);
+	pthread_mutex_lock(&philo->queue->msg_mutex);
+	count = philo->queue->count;
+	philo->queue->time[1][count] = time;
+	philo->queue->philo[1][count] = philo->philo_id;
+	philo->queue->action[1][count] = msg;
+	(philo->queue->count)++;
+	pthread_mutex_unlock(&philo->queue->msg_mutex);
 }
