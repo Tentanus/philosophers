2acdd2de30bffac6b1c6a70669cefc3e810b189a
Martijn Weverling <mweverli@student.codam.nl>
2023-06-05 19:56:03 +0200

    moved go_* to separate file and removed redundant checks

--- src/philo/philo_routine.c
+++ src/philo/philo_routine.c
@@ -6,7 +6,7 @@
 /*   By: mweverli <mweverli@student.codam.n>          +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/05/29 17:47:49 by mweverli      #+#    #+#                 */
-/*   Updated: 2023/06/03 20:13:28 by mweverli      ########   odam.nl         */
+/*   Updated: 2023/06/05 19:50:05 by mweverli      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,55 +15,26 @@
 typedef void				(*t_go_action) \
 							(t_philo *philo, const int64_t sim_start);
 
-void	go_eat(t_philo *philo, const int64_t sim_start)
+static bool	check_err(t_philo *philo)
 {
-	int64_t	start_eat;
+	bool	ret;
 
-	pthread_mutex_lock(philo->fork_r);
-//	philo_queue_message(philo, time_diff_ms(sim_start, time_of_day_ms()), FORK);
-	pthread_mutex_lock(philo->fork_l);
-//	philo_queue_message(philo, time_diff_ms(sim_start, time_of_day_ms()), FORK);
-//	philo_queue_message(philo, time_diff_ms(sim_start, start_eat), EAT);
-	if (philo->public_data->err == true)
-	{
-		pthread_mutex_unlock(philo->fork_r);
-		pthread_mutex_unlock(philo->fork_l);
-		pthread_mutex_unlock(&philo->eating);
-		return ;
-	}
-	start_eat = time_of_day_ms();
-	printf(FORMAT_MSG, time_diff_ms(sim_start, start_eat), philo->philo_id, "is eating");
 	pthread_mutex_lock(&philo->eating);
-	philo->time_last_meal = start_eat;
-	philo->nbr_meal_eaten += 1;
-	time_sleep_ms(philo->public_data->time_eat);// - 			time_diff_ms(start_eat, time_of_day_ms()));
-	pthread_mutex_unlock(philo->fork_r);
-	pthread_mutex_unlock(philo->fork_l);
+	ret = philo->public_data->err;
 	pthread_mutex_unlock(&philo->eating);
-	philo->status = SLEEP;
+	return (ret);
 }
 
-void	go_sleep(t_philo *philo, const int64_t sim_start)
+static bool	start_sequence(t_philo *philo, int64_t *sim_start)
 {
-	const int64_t	start_sleep = time_of_day_ms();
+	bool	err;
 
-//	philo_queue_message(philo, time_diff_ms(sim_start, start_sleep), SLEEP);
-	if (philo->public_data->err == true)
-		return ;
-	printf(FORMAT_MSG, time_diff_ms(sim_start, start_sleep), philo->philo_id, "is sleeping");
-	time_sleep_ms(philo->public_data->time_sleep);// - 		time_diff_ms(start_sleep, time_of_day_ms()));
-	philo->status = THINK;
-}
-
-void	go_think(t_philo *philo, const int64_t sim_start)
-{
-	const int64_t	start_think = time_of_day_ms();
-
-//	philo_queue_message(philo, time_diff_ms(sim_start, start_think), THINK);
-	if (philo->public_data->err == true)
-		return ;
-	printf(FORMAT_MSG, time_diff_ms(sim_start, start_think), philo->philo_id, "is thinking");
-	philo->status = EAT;
+	pthread_mutex_lock(&philo->eating);
+	*sim_start = philo->public_data->time_start;
+	philo->time_last_meal = *sim_start;
+	err = philo->public_data->err;
+	pthread_mutex_unlock(&philo->eating);
+	return (err);
 }
 
 static const t_go_action	g_func[4] = {
@@ -77,13 +48,13 @@ void	*philo_routine(void *ptr)
 {
 	t_philo	*philo;
 	int64_t	sim_start;
+	bool	err;
 
 	philo = ptr;
 	pthread_mutex_lock(&philo->public_data->start);
 	pthread_mutex_unlock(&philo->public_data->start);
-	sim_start = philo->public_data->time_start;
-	philo->time_last_meal = sim_start;
-	if (philo->public_data->err == true)
+	err = start_sequence(philo, &sim_start);
+	if (err == true)
 		return (NULL);
 	if (philo->philo_id % 2 == 1)
 	{
@@ -92,19 +63,10 @@ void	*philo_routine(void *ptr)
 	}
 	while (1)
 	{
-		if (philo->public_data->err == true)
+		err = check_err(philo);
+		if (err == true)
 			break ;
-		else if (philo->nbr_meal_eaten == philo->public_data->nbr_meal)
-		{
-			philo->public_data->nbr_full_philo += 1;
-			if (philo->public_data->nbr_full_philo != philo->public_data->nbr_philo)
-				go_think(philo, sim_start);
-			else
-				printf("\n%d %d philos full, ending sim\n", time_diff_ms(sim_start, time_of_day_ms()), philo->public_data->nbr_philo);
-			break ;
-		}
 		g_func[philo->status](philo, sim_start);
 	}
-//	printf("philo [%d] terminated at %d\n", philo->philo_id, time_diff_ms(sim_start, time_of_day_ms()));
 	return (NULL);
 }
